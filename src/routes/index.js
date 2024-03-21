7a2c74cd5fc16fdfc93de021c11cfdee67263b3f
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-20 23:05:04 -0300

    image upload finished

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
-const { Router } = require('express')
+const { Router, static } = require('express')
 const routes = Router()
 const usersRoutes = require('./users.routes')
 const notesRoutes = require('./notes.routes')
 const tagsRoutes = require('./tags.routes')
 const sessionsRoutes = require('./sessions.routes')
+const { UPLOADS_FOLDER } = require('../config/upload')
 
+routes.use("/files", static(UPLOADS_FOLDER))
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
 routes.use("/notes", notesRoutes)
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,21 @@
 const { Router } = require('express')
 const usersRoutes = Router()
-const UsersController = require("../controllers/UsersController")
-const ensureAthenticated = require("../middlewares/ensureAthenticated")
 
+const UsersController = require("../controllers/UsersController")
 const usersController = new UsersController()
 
-usersRoutes.post("/", usersController.create)
+const UserAvatarController = require("../controllers/UserAvatarController")
+const userAvatarController = new UserAvatarController()
 
+const ensureAthenticated = require("../middlewares/ensureAthenticated")
+
+const multer = require("multer")
+const uploadConfig = require("../config/upload")
+const upload = multer(uploadConfig.MULTER)
+
+
+usersRoutes.post("/", usersController.create)
 usersRoutes.put("/", ensureAthenticated, usersController.update)
+usersRoutes.patch("/avatar", ensureAthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes
