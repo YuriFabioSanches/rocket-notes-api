82aa2264b306ca181ea299126dd386610e73ffc7
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-22 14:14:08 -0300

    cors installed and added to server.js

--- src/server.js
+++ src/server.js
@@ -3,13 +3,17 @@ const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require("./utils/AppError")
 const routes = require("./routes");
 const express = require("express");
-const app = express();
+const cors = require("cors");
 
 migrationsRun()
 
-app.use(express.json())
+const app = express();
 
-app.use(routes)
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
@@ -17,8 +21,8 @@ app.use((error, request, response, next) => {
       status: "error",
       statusCode: error.statusCode,
       message: error.message
-    })
-  }
+    });
+  };
 
   console.error(error)
 
@@ -26,8 +30,8 @@ app.use((error, request, response, next) => {
     status: "error",
     statusCode: 500,
     message: "Internal server error"
-  })
-})
+  });
+});
 
 const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
