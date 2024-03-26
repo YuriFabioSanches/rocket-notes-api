721e60475db34e577746d798969c813a171250a8
Yuri Fabio Sanches <yuri.fsanches@hotmail.com>
2024-03-25 23:37:51 -0300

    group by filter added to get all notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -20,6 +20,7 @@ class NotesController {
       .whereLike('notes.title', `%${title}%`)
       .whereIn("name", filterTags)
       .innerJoin("notes", "notes.id", "tags.note_id")
+      .groupBy("notes.id")
       .orderBy("notes.created_at")
     }else {
       notes = await knex("notes")
